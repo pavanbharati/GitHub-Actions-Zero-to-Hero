a6678f1ff7d6656da0588357d6885cdc256b3cbc
pavanbharati <pavanbharati1987@gmail.com>
2024-10-29 22:54:13 -0700

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,11 @@
 # app.py
-# This is a test commit-11
+# This is a test commit
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+def test_sub():
+    assert sub(1, 2) == 1
+    
